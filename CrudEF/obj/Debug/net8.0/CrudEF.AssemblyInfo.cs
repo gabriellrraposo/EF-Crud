23853f6c8892bbf70a33cb5fcb09d3d148ebec75
GABRIEL LUCAS ROSA RAPOSO <12100498@aluno.cotemig.com.br>
2024-07-04 20:14:26 -0300

    Adiciona gitignore

--- CrudEF/obj/Debug/net8.0/CrudEF.AssemblyInfo.cs
+++ CrudEF/obj/Debug/net8.0/CrudEF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrudEF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae28d53a75de7914aee899ec59144d4a3f8be087")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrudEF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrudEF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
