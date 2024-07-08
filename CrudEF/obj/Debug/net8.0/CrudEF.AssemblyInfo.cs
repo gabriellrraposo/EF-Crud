da171aab327c6a1cf35980b35582d60a4c7a193d
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2024-07-08 17:29:20 -0300

    Adiciona mensagens de erro e finaliza a inserção no bd

--- CrudEF/obj/Debug/net8.0/CrudEF.AssemblyInfo.cs
+++ CrudEF/obj/Debug/net8.0/CrudEF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrudEF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae28d53a75de7914aee899ec59144d4a3f8be087")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d69bb20328555313369443bf1df629d3ff975d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrudEF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrudEF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
